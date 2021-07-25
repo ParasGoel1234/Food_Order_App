a2930daac2df4a350b468fff5fa8d0cd0f926ee2
Paras Goel <65220753+ParasGoel1234@users.noreply.github.com>
2021-07-25 21:43:23 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,60 +1,114 @@
-package com.codewithparas.foodorderapp;
+package com.codewithparas.whatsapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-import com.codewithparas.foodorderapp.Adapters.MainAdapters;
-import com.codewithparas.foodorderapp.Models.MainModels;
-import com.codewithparas.foodorderapp.databinding.ActivityMainBinding;
+import com.codewithparas.whatsapp.Model.Users;
+import com.codewithparas.whatsapp.fregment.UsersFragment;
+import com.codewithparas.whatsapp.fregment.chatsFragment;
+import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMainBinding binding;
+    ViewPager viewPager;
+    TabLayout tabLayout;
+    FirebaseUser firebaseUser;
+    DatabaseReference myRef;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main);
 
-        ArrayList<MainModels>list = new ArrayList<>();
-        list.add(new MainModels(R.drawable.image1, "Extra chess burger starting at rupees", "45", "Burger"));
-        list.add(new MainModels(R.drawable.pizza, "Extra chess pizza starting at rupees", "69", "Pizza"));
-        list.add(new MainModels(R.drawable.dosa, "south indian food", "99", "Dosa"));
-        list.add(new MainModels(R.drawable.shahi, "shahi Paneer with rooti", "120", "Shahi Paneer"));
-        list.add(new MainModels(R.drawable.momos, "fried and steamed momos starting at rupees", "40", "Momos"));
-        list.add(new MainModels(R.drawable.chowmein, "Veg Chowmein snakes items ", "80", "Chowmein"));
-        list.add(new MainModels(R.drawable.largeburger, "combo Pack of 4 burger", "200", "Large Burger"));
-        list.add(new MainModels(R.drawable.pizza, "All snakes items are available starting at rupees", "69", "Pizza"));
-        list.add(new MainModels(R.drawable.dosa, "south indian food Idli and Dosa starting at rupees", "99", "Dosa"));
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        myRef = FirebaseDatabase.getInstance().getReference("MyUser").child(firebaseUser
+                .getUid());
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Users users = snapshot.getValue(Users.class);
+                Toast.makeText(MainActivity.this, "User Login:"+users.getUserName(), Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
 
-        MainAdapters adepter = new MainAdapters(list, this);
-        binding.recyclerview.setAdapter(adepter);
+            }
+        });
 
-        LinearLayoutManager LayoutManager = new LinearLayoutManager(this);
-        binding.recyclerview.setLayoutManager(LayoutManager);
-        }
+        viewPager  = findViewById(R.id.view_pager);
+        tabLayout = findViewById(R.id.TabLayout);
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+        viewPagerAdapter.addFragment(new chatsFragment(), "Chats");
+        viewPagerAdapter.addFragment(new UsersFragment(), "Users");
+
+
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.menu, menu);
-        return super.onCreateOptionsMenu(menu);
+        return true;
     }
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.Logout:
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch(item.getItemId()){
-            case R.id.orders:
-                startActivity(new Intent(MainActivity.this, OrderActivity.class ));
-
+                return true;
         }
-        return super.onOptionsItemSelected(item);
+        return false;
     }
+
+   class ViewPagerAdapter extends FragmentPagerAdapter{
+        private ArrayList<Fragment>fragments;
+        private ArrayList<String>titles;
+
+       public ViewPagerAdapter(@NonNull FragmentManager fm) {
+           super(fm);
+           this.fragments = new ArrayList<>() ;
+           this.titles = new ArrayList<>() ;
+       }
+        public Fragment getItem(int position){
+           return fragments.get(position);
+        }
+        @Override
+       public int getCount() {
+           return fragments.size(); }
+
+        public void addFragment(Fragment fragment, String title){
+           fragments.add(fragment);
+           titles.add(title);
+        }
+
+       @Nullable
+       @Override
+       public CharSequence getPageTitle(int position) {
+           return titles.get(position);
+       }
+   }
 }
